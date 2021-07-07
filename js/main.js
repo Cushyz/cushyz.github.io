92ef5ea230a948f6192b215eddcc4c7466ed0880
cushyz <cszcsz123@outlook.com>
2021-07-07 13:41:50 +0800

    new theme

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var mnhd = true;
+var sdhd = true;
+window.onload = function () {
+    let nbt = document.getElementById("navbt");
+    let sbt = document.getElementById("sidebt");
+    let li = document.getElementById("navli");
+    let sd = document.getElementById("cright");
+    nbt.addEventListener("click", () => {
+        if (mnhd)
+            li.classList.add("show");
+        else
+            li.classList.remove("show");
+        mnhd = !mnhd;
+    });
+    sbt.addEventListener("click", () => {
+        if (sdhd) {
+            sd.classList.add("show");
+            sbt.classList.add("clicked");
+        }
+        else {
+            sd.classList.remove("show");
+            sbt.classList.remove("clicked");
+        }
+        sdhd = !sdhd;
+    });
+}
